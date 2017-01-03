6f172fe33be8dbf561d323683dff43209fd29abe
Jinlin Li <jinlin.li@geneegroup.com>
2017-01-03 14:06:03 +0800

    增加tag db api

--- class/Gini/Controller/API/TagDB/Data.php
+++ class/Gini/Controller/API/TagDB/Data.php
@@ -4,6 +4,36 @@ namespace Gini\Controller\API\TagDB;
 
 class Data extends \Gini\Controller\API\Base
 {
+    public function actionSearchTags($criteria)
+    {
+        $token = md5(J($criteria));
+        $keyword = $criteria['keyword'];
+        $_SESSION[$token] = $criteria;
+        $db = \Gini\Database::db();
+        $count = $db->value("SELECT count(*) FROM `tag` WHERE `name` LIKE ".$db->quote('%'.$keyword.'%'));
+        $data = [
+            'total' => $count,
+            'token' => $token,
+        ];
+        return $data;
+
+    }
+
+    public function actionGetTags($token, $start = 0, $limit = 20)
+    {
+        $db = \Gini\Database::db();
+        $criteria = $_SESSION[$token];
+        $result = [];
+        $keyword = $criteria['keyword'];
+        $rows = $db->query("SELECT * FROM `tag` WHERE `name` LIKE ".$db->quote('%'.$keyword.'%')." LIMIT {$start}, {$limit}")->rows();
+        foreach ($rows as $row) {
+            $result[] = [
+                'name' => $row->name,
+                'data' => $row->data,
+            ];
+        }
+        return $result;
+    }
     public function actionGet($name)
     {
         $tag = a('tag', ['name'=> $name]);
